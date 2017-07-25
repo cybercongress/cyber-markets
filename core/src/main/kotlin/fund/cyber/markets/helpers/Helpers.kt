b4e048ebd371bfc0b3e5583126a9002647d335a3
Ruslan Ibragimov <ruslan@ibragimov.by>
2017-07-26 00:15:17 +0300

    Create aliases for coroutines builders with default context.

--- core/src/main/kotlin/fund/cyber/markets/helpers/Helpers.kt
+++ core/src/main/kotlin/fund/cyber/markets/helpers/Helpers.kt
@@ -1,15 +1,22 @@
 package fund.cyber.markets.helpers
 
+import fund.cyber.markets.applicationContext
+import fund.cyber.markets.applicationSingleThreadContext
+import kotlinx.coroutines.experimental.CoroutineScope
+import kotlinx.coroutines.experimental.CoroutineStart
+import kotlinx.coroutines.experimental.Job
 import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
 import org.slf4j.LoggerFactory
 import java.util.concurrent.TimeUnit
+import kotlin.coroutines.experimental.CoroutineContext
 
 
 private object Helpers {
     val LOGGER = LoggerFactory.getLogger(Helpers::class.java)!!
 }
 
-suspend fun <T, R> T.retryUntilSuccess(retryDelay: Long = 5, block: suspend () -> R): R {
+suspend fun <R> retryUntilSuccess(retryDelay: Long = 5, block: suspend () -> R): R {
     while (true) {
         try {
             return block()
@@ -19,3 +26,15 @@ suspend fun <T, R> T.retryUntilSuccess(retryDelay: Long = 5, block: suspend () -
         }
     }
 }
+
+fun concurrent(
+    context: CoroutineContext = applicationContext,
+    start: CoroutineStart = CoroutineStart.DEFAULT,
+    block: suspend CoroutineScope.() -> Unit
+): Job = launch(context, start, block)
+
+fun local(
+    context: CoroutineContext = applicationSingleThreadContext,
+    start: CoroutineStart = CoroutineStart.DEFAULT,
+    block: suspend CoroutineScope.() -> Unit
+): Job = launch(context, start, block)
