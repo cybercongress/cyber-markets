b2b82f4a527efa942879a76921f868fb38099f13
hleb-albau <hleb.albau@gmail.com>
2017-07-26 01:56:02 +0300

    Undertow await for ws connection creation

--- core/src/main/kotlin/fund/cyber/markets/helpers/Extensions.kt
+++ core/src/main/kotlin/fund/cyber/markets/helpers/Extensions.kt
@@ -10,10 +10,10 @@ import kotlin.coroutines.experimental.suspendCoroutine
 suspend fun <T> IoFuture<T>.cAwait(): T {
 
     return suspendCoroutine { cont: Continuation<T> ->
-        val notifier = Notifier<T, Any?> { ioFuture, _ ->
-            when (ioFuture.status) {
-                DONE -> cont.resume(get())
-                FAILED -> cont.resumeWithException(exception)
+        val notifier = Notifier<T, Any?> { future, _ ->
+            when (future.status) {
+                DONE -> cont.resume(future.get())
+                FAILED -> cont.resumeWithException(future.exception)
                 CANCELLED -> cont.resumeWithException(IllegalStateException("Canceled operation"))
                 WAITING -> cont.resumeWithException(IllegalStateException("Awake during waiting operation"))
                 null -> cont.resumeWithException(IllegalStateException("Unknown future state"))
