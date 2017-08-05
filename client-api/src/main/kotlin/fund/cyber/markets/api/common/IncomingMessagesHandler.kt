b58a80d6b9776736ccc69bee076c76b880262905
hleb-albau <hleb.albau@gmail.com>
2017-08-05 19:50:43 +0300

    provide api fo queering trades by pairs

--- client-api/src/main/kotlin/fund/cyber/markets/api/common/IncomingMessagesHandler.kt
+++ client-api/src/main/kotlin/fund/cyber/markets/api/common/IncomingMessagesHandler.kt
@@ -1,20 +1,27 @@
 package fund.cyber.markets.api.common
 
+import fund.cyber.markets.api.trades.TradesBroadcastersIndex
 import io.undertow.websockets.core.AbstractReceiveListener
 import io.undertow.websockets.core.BufferedTextMessage
 import io.undertow.websockets.core.StreamSourceFrameChannel
 import io.undertow.websockets.core.WebSocketChannel
 
-class IncomingMessagesHandler : AbstractReceiveListener() {
+class IncomingMessagesHandler(
+        private val tradesBroadcastersIndex: TradesBroadcastersIndex
+) : AbstractReceiveListener() {
 
     private val commandsParser = WebSocketCommandsParser()
 
 
     override fun onFullTextMessage(wsChannel: WebSocketChannel, bufferedMessage: BufferedTextMessage) {
-        val message = bufferedMessage.data
-        when (commandsParser.parseMessage(message)) {
-            is UnknownMessage -> {}
-            is TradeChannelSubscriptionCommand -> {}
+        val command = commandsParser.parseMessage(bufferedMessage.data)
+        when (command) {
+            is UnknownCommand -> {}
+            is TradeChannelSubscriptionCommand -> {
+                tradesBroadcastersIndex.broadcastersFor(command.pairs).forEach { broadcaster ->
+                    broadcaster.registerChannel(wsChannel)
+                }
+            }
         }
     }
 
--- client-api/src/main/kotlin/fund/cyber/markets/api/common/RootWebSocketHandler.kt
+++ client-api/src/main/kotlin/fund/cyber/markets/api/common/RootWebSocketHandler.kt
@@ -1,13 +1,14 @@
 package fund.cyber.markets.api.common
 
 import io.undertow.websockets.WebSocketConnectionCallback
+import io.undertow.websockets.core.AbstractReceiveListener
 import io.undertow.websockets.core.WebSocketChannel
 import io.undertow.websockets.spi.WebSocketHttpExchange
 
 
-class RootWebSocketHandler : WebSocketConnectionCallback {
-
-    private val incomingMessagesHandler = IncomingMessagesHandler()
+class RootWebSocketHandler(
+        private val incomingMessagesHandler: AbstractReceiveListener
+) : WebSocketConnectionCallback {
 
     override fun onConnect(exchange: WebSocketHttpExchange, channel: WebSocketChannel) {
         channel.getReceiveSetter().set(incomingMessagesHandler)
