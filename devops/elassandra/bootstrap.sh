8a82acc549891140e27cb25f742b875c1b5278e8
Hleb Albau <hleb.albau@gmail.com>
2017-09-05 21:48:16 +0300

    Elassandra manage cluster options

--- devops/elassandra/bootstrap.sh
+++ devops/elassandra/bootstrap.sh
@@ -18,3 +18,16 @@ curl -XPUT 'http://localhost:9200/blockchains/_mapping/ethereum' -d '{
 # search
 curl -XGET 'http://localhost:9200/blockchains/ethereum/1'
 curl -XGET 'http://localhost:9200/blockchains/_mapping/ethereum?pretty=true'
+
+curl -XGET 'localhost:9200/blockchains/_search?pretty'-H 'Content-Type: application/json' -d '{
+  "query": {
+    "match": {
+      "rawblock": "0x62e4bacc5efb0941bf8784e431746b95b14a700eb7b1ecc81679e4b496b3fe3c"
+    }
+  }
+}'
+
+docker exec -it localelassandra_elassandra-1_1 bash
+
+nodetool cfhistograms
+nodetool status
