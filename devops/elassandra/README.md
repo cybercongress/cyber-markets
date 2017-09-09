20f79592492fb83392e8c6095bf6ab4f2f943746
Hleb Albau <hleb.albau@gmail.com>
2017-09-09 19:26:24 +0300

    Add elassandra development quick start

---
+++ devops/elassandra/README.md
@@ -0,0 +1,38 @@
+# Run elassandra for dev
+
+### Cassandra tool
+
+* [Download cassandra](http://cassandra.apache.org/download/ )
+* [Add cassandra bin to $PATH](https://stackoverflow.com/questions/29944484/how-to-run-cassandra-cqlsh-from-anywhere)
+
+
+### First run
+
+```bash
+
+# Run elassandra
+sudo docker-compose up -d
+
+# Initialize keyspaces and tables
+cqlsh  -f ./bootstrap.cql
+
+# Add elasstic indexes
+sh bootstrap.sh 
+```
+
+### Useful commands
+
+See cluster status
+```bash
+nodetool status
+```
+
+Dive into elassandra docker container shell
+```bash
+docker exec -it elassandra_elassandra_1 bash
+```
+
+
+
+
+
--- devops/elassandra/bootstrap.sh
+++ devops/elassandra/bootstrap.sh
@@ -1,41 +1,7 @@
 #!/usr/bin/env bash
 
-# mapping
-curl -XPUT "http://localhost:9200/blockchains/" -d '{
-   "settings" : { "keyspace" : "blockchains" } },
+# Create elastic index
+
+curl -XPUT "http://localhost:9200/markets/" -d '{
+   "settings" : { "keyspace" : "markets" } },
 }'
-
-
-curl -XPUT 'http://localhost:9200/blockchains/_mapping/ethereum' -d '{
-        "ethereum" : {
-            "properties" : {
-              "rawblock" : { "type" : "string", "index" : "analyzed","cql_collection" : "singleton"}
-            }
-        }
-}'
-
-curl -XPUT 'http://localhost:9200/blockchains/_mapping/bitcoin' -d '{
-        "bitcoin" : {
-            "properties" : {
-              "rawblock" : { "type" : "string", "index" : "analyzed","cql_collection" : "singleton"}
-            }
-        }
-}'
-
-
-# search
-curl -XGET 'http://localhost:9200/blockchains/ethereum/1';
-curl -XGET 'http://localhost:9200/blockchains/_mapping/ethereum?pretty=true'
-
-curl -XGET 'localhost:9200/blockchains/_search?pretty' -H 'Content-Type: application/json' -d '{
-  "query": {
-    "match": {
-      "rawblock": "ehtereum 00000000a5ae2c2c8b85706a041592fd81b9d3f4f56e8e760999003bd1eaed93"
-    }
-  }
-}'
-
-docker exec -it localelassandra_elassandra-1_1 bash
-
-nodetool cfhistograms
-nodetool status
