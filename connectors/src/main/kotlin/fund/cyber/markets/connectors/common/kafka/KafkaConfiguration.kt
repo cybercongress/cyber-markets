cabab0bbc90f2b8e783e023bbff260f07f967948
Ruslan Ibragimov <ruslan@ibragimov.by>
2017-09-20 20:59:36 +0300

    #50 [spark] create live time trades ticker calculation

--- connectors/src/main/kotlin/fund/cyber/markets/connectors/common/kafka/KafkaConfiguration.kt
+++ connectors/src/main/kotlin/fund/cyber/markets/connectors/common/kafka/KafkaConfiguration.kt
@@ -1,7 +1,7 @@
 package fund.cyber.markets.connectors.common.kafka
 
 import fund.cyber.markets.helpers.env
-import java.util.*
+import java.util.Properties
 
 class KafkaConfiguration(
         val kafkaServers: String = env("KAFKA_CONNECTION", "localhost:9092")
@@ -9,6 +9,7 @@ class KafkaConfiguration(
     fun tradesConsumersProperties(groupId: String): Properties {
         return Properties().apply {
             put("bootstrap.servers", kafkaServers)
+            put("max.request.size", 126358200)
             put("group.id", groupId)
         }
     }
