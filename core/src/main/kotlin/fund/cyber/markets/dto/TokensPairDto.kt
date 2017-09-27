7c1eb2fc4b79676ba20ad95b7eafbc7873c46ad8
Max Gergalov <mgergalov@gmail.com>
2017-09-27 10:29:12 +0300

    feature/cyber-markets#71:  - override equals() and hashCode() for TokenPairdto

--- core/src/main/kotlin/fund/cyber/markets/dto/TokensPairDto.kt
+++ core/src/main/kotlin/fund/cyber/markets/dto/TokensPairDto.kt
@@ -6,4 +6,24 @@ package fund.cyber.markets.dto
 class TokensPairDto(
         val base: String,
         val quote: String
-)
+){
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is TokensPairDto) return false
+
+        if (base != other.base) return false
+        if (quote != other.quote) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = base.hashCode()
+        result = 31 * result + quote.hashCode()
+        return result
+    }
+
+    override fun toString(): String {
+        return "TokensPairDto(base='$base', quote='$quote')"
+    }
+}
