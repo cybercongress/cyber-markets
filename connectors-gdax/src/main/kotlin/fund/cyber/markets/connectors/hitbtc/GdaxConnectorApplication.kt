afd4d8cfd29ef3d2273b49f7ac2598df520f54ba
Max Gergalov <mgergalov@gmail.com>
2017-11-30 00:29:38 +0300

    feature/cyber-markets #101 : - add trades from Gdax; - add orders from GGdax; - create cm-connectors-gdax docker image; - update circle ci;

---
+++ connectors-gdax/src/main/kotlin/fund/cyber/markets/connectors/hitbtc/GdaxConnectorApplication.kt
@@ -0,0 +1,49 @@
+package fund.cyber.markets.connectors.hitbtc
+
+import fund.cyber.markets.connectors.common.kafka.ConnectorKafkaProducer
+import fund.cyber.markets.connectors.common.kafka.OrdersUpdateProducerRecord
+import fund.cyber.markets.connectors.common.kafka.TradeProducerRecord
+import fund.cyber.markets.connectors.hitbtc.connector.GdaxOrdersEndpoint
+import fund.cyber.markets.connectors.hitbtc.connector.GdaxTradesEndpoint
+import fund.cyber.markets.connectors.helpers.concurrent
+import fund.cyber.markets.model.OrdersBatch
+import fund.cyber.markets.model.Trade
+
+val supportedTradesEndpoints = listOf(
+        GdaxTradesEndpoint()
+)
+val supportedOrdersEndpoints = listOf(
+        GdaxOrdersEndpoint()
+)
+val tradeKafkaProducer = ConnectorKafkaProducer<Trade>()
+val orderKafkaProducer = ConnectorKafkaProducer<OrdersBatch>()
+fun main(args: Array<String>) {
+    val debugMode = System.getProperty("debug") != null
+
+    supportedTradesEndpoints.forEach { exchange ->
+        concurrent {
+            val dataChannel = exchange.subscribe()
+            concurrent {
+                while (true) {
+                    val message = dataChannel.receive()
+                    message.trades.forEach { trade ->
+                        if (debugMode) println(trade) else tradeKafkaProducer.send(TradeProducerRecord(trade))
+                    }
+                }
+            }
+        }
+    }
+
+    supportedOrdersEndpoints.forEach { exchange ->
+        concurrent {
+            val dataChannel = exchange.subscribe()
+            concurrent {
+                while (true) {
+                    val message = dataChannel.receive()
+                    if (debugMode) println(message)
+                    else orderKafkaProducer.send(OrdersUpdateProducerRecord(message.ordersBatch))
+                }
+            }
+        }
+    }
+}
---
+++ connectors-gdax/src/main/kotlin/fund/cyber/markets/connectors/hitbtc/connector/GdaxEndpoint.kt
@@ -0,0 +1,48 @@
+package fund.cyber.markets.connectors.hitbtc.connector
+
+import fund.cyber.markets.connectors.common.ExchangeMessage
+import fund.cyber.markets.connectors.common.GDAX_WS_ENDPOINT
+import fund.cyber.markets.connectors.common.ws.OrdersWsEndpoint
+import fund.cyber.markets.connectors.common.ws.TradesWsEndpoint
+import fund.cyber.markets.connectors.common.ws.pusher.PusherMessage
+import org.slf4j.LoggerFactory
+
+class GdaxTradesEndpoint : TradesWsEndpoint(GDAX_WS_ENDPOINT) {
+    private val LOGGER = LoggerFactory.getLogger(GdaxTradesEndpoint::class.java)!!
+
+    override val name: String = "GDAX Trades"
+    override val messageParser = GdaxTradesMessageParser(channelSymbolForTokensPairs)
+    override val pairsProvider = GdaxPairsProvider()
+
+    override fun getSubscriptionMsgByChannelSymbol(pairSymbol: String): String {
+        return """{"type":"subscribe","channels":[{"name":"matches","product_ids":["$pairSymbol"]}]}"""
+    }
+
+    override fun handleUnknownMessage(message: ExchangeMessage) {
+        if (message is PusherMessage) {
+            LOGGER.debug(message.message(name))
+        } else {
+            super.handleUnknownMessage(message)
+        }
+    }
+}
+
+class GdaxOrdersEndpoint : OrdersWsEndpoint(GDAX_WS_ENDPOINT) {
+    private val LOGGER = LoggerFactory.getLogger(GdaxOrdersEndpoint::class.java)!!
+
+    override val name: String = "GDAX Orders"
+    override val messageParser = GdaxOrdersMessageParser(channelSymbolForTokensPairs)
+    override val pairsProvider = GdaxPairsProvider()
+
+    override fun getSubscriptionMsgByChannelSymbol(pairSymbol: String): String {
+        return """{"type":"subscribe","channels":[{"name":"level2_50","product_ids":["$pairSymbol"]}]}"""
+    }
+
+    override fun handleUnknownMessage(message: ExchangeMessage) {
+        if (message is PusherMessage) {
+            LOGGER.debug(message.message(name))
+        } else {
+            super.handleUnknownMessage(message)
+        }
+    }
+}
