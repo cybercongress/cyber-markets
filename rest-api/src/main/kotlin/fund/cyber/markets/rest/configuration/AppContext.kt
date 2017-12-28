6cf3379849e61bd1233bf409df6a41889fca8363
Valentin Stavetski <staveckij.v@gmail.com>
2017-12-28 15:51:37 +0300

    #97 use own ObjectMapper for each handler

--- rest-api/src/main/kotlin/fund/cyber/markets/rest/configuration/AppContext.kt
+++ rest-api/src/main/kotlin/fund/cyber/markets/rest/configuration/AppContext.kt
@@ -1,13 +1,8 @@
 package fund.cyber.markets.rest.configuration
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import fund.cyber.markets.cassandra.CassandraService
 
 object AppContext {
-
-    val jsonSerializer = ObjectMapper()
-
     val cassandraService = CassandraService(RestApiConfiguration.cassandraProperties)
-
     val tickerRepository by lazy { cassandraService.tickerRepository }
 }
