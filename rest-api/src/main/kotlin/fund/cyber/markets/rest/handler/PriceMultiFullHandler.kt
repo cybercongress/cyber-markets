7e202601f3ab4c294617525d156fb4dcf655888b
Valentin Stavetski <staveckij.v@gmail.com>
2018-02-12 18:48:08 +0300

    #136 add supply, market capitalization to pricemultifull

--- rest-api/src/main/kotlin/fund/cyber/markets/rest/handler/PriceMultiFullHandler.kt
+++ rest-api/src/main/kotlin/fund/cyber/markets/rest/handler/PriceMultiFullHandler.kt
@@ -1,8 +1,11 @@
 package fund.cyber.markets.rest.handler
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import fund.cyber.markets.cassandra.repository.SupplyRepository
 import fund.cyber.markets.cassandra.repository.TickerRepository
 import fund.cyber.markets.cassandra.repository.VolumeRepository
 import fund.cyber.markets.common.Durations
+import fund.cyber.markets.common.UpperCaseNamingStrategy
 import fund.cyber.markets.dto.TokensPair
 import fund.cyber.markets.helpers.MILLIS_TO_SECONDS
 import fund.cyber.markets.helpers.booleanValue
@@ -16,11 +19,19 @@ import fund.cyber.markets.rest.model.PriceMultiFullData
 import fund.cyber.markets.rest.model.PriceMultiFullModel
 import io.undertow.server.HttpHandler
 import io.undertow.server.HttpServerExchange
+import io.undertow.util.Headers
+import java.math.BigDecimal
 
 class PriceMultiFullHandler(
-    private val tickerRepository: TickerRepository = AppContext.tickerRepository,
-    private val volumeRepository: VolumeRepository = AppContext.volumeRepository
-    ) : AbstractHandler(), HttpHandler {
+        private val jsonSerializer: ObjectMapper = ObjectMapper(),
+        private val tickerRepository: TickerRepository = AppContext.tickerRepository,
+        private val volumeRepository: VolumeRepository = AppContext.volumeRepository,
+        private val supplyRepository: SupplyRepository = AppContext.supplyRepository
+) : AbstractHandler(), HttpHandler {
+
+    init {
+        jsonSerializer.propertyNamingStrategy = UpperCaseNamingStrategy()
+    }
 
     override fun handleRequest(httpExchange: HttpServerExchange) {
 
@@ -46,6 +57,8 @@ class PriceMultiFullHandler(
                     continue
                 }
 
+                val supply = supplyRepository.get(base)
+
                 var ticker24h: Ticker? = null
                 var closePrice = tickerRepository.getTicker(TokensPair(base, quote), Durations.MINUTE, exchange, timestamp)?.close
                 if (closePrice == null && tryConversion) {
@@ -63,6 +76,7 @@ class PriceMultiFullHandler(
                 var priceData: PriceMultiFullData? = null
 
                 if (closePrice != null && ticker24h != null) {
+                    val change = ticker24h.close.minus(ticker24h.open).div(ticker24h.open).multiply(BigDecimal(100L))
                     priceData = PriceMultiFullData(
                             exchange,
                             base,
@@ -73,7 +87,11 @@ class PriceMultiFullHandler(
                             volumeQuote?.value,
                             ticker24h.open,
                             ticker24h.maxPrice,
-                            ticker24h.minPrice
+                            ticker24h.minPrice,
+                            ticker24h.close.minus(ticker24h.open),
+                            change,
+                            supply?.value,
+                            supply?.value!!.multiply(closePrice)
                     )
                 } else if (closePrice != null) {
                     priceData = PriceMultiFullData(
@@ -86,7 +104,11 @@ class PriceMultiFullHandler(
                             volumeQuote?.value,
                             closePrice,
                             closePrice,
-                            closePrice
+                            closePrice,
+                            BigDecimal.ZERO,
+                            BigDecimal.ZERO,
+                            supply?.value,
+                            supply?.value!!.multiply(closePrice)
                     )
                 }
 
@@ -103,6 +125,11 @@ class PriceMultiFullHandler(
         }
 
         send(PriceMultiFullModel(raw), httpExchange)
+    }
+
+    override fun send(result: Any, httpExchange: HttpServerExchange) {
+        httpExchange.responseHeaders.put(Headers.CONTENT_TYPE, "application/json")
+        httpExchange.responseSender.send(jsonSerializer.writeValueAsString(result))
     }
 
 }
