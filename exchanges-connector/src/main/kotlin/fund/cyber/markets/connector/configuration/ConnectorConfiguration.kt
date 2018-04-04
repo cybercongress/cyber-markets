babeaab3d20fbd99e1c97832c8ac9dba18193377
Valentin Stavetski <staveckij.v@gmail.com>
2018-04-04 13:30:10 +0300

    #151 - add binance, etherdelta exchanges to default configuration

--- exchanges-connector/src/main/kotlin/fund/cyber/markets/connector/configuration/ConnectorConfiguration.kt
+++ exchanges-connector/src/main/kotlin/fund/cyber/markets/connector/configuration/ConnectorConfiguration.kt
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 
 const val EXCHANGES_PROP = "EXCHANGES"
-const val EXCHANGES_PROP_DEFAULT = "bitfinex,bitstamp,gdax,hitbtc,poloniex"
+const val EXCHANGES_PROP_DEFAULT = "bitfinex,bitstamp,gdax,hitbtc,poloniex,binance,etherdelta"
 
 const val PARITY_URL = "PARITY_URL"
 const val PARITY_URL_DEFAULT = "http://127.0.0.1:8545"
