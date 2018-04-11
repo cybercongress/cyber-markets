7e376539649f4690c21b088b5cb34f7e37d0c062
Valentin Stavetski <staveckij.v@gmail.com>
2018-04-11 16:58:51 +0300

    #161 add retry listener for logging

--- exchanges-connector/src/main/kotlin/fund/cyber/markets/connector/configuration/RetryPolicyConfiguration.kt
+++ exchanges-connector/src/main/kotlin/fund/cyber/markets/connector/configuration/RetryPolicyConfiguration.kt
@@ -1,11 +1,26 @@
 package fund.cyber.markets.connector.configuration
 
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.retry.RetryCallback
+import org.springframework.retry.RetryContext
 import org.springframework.retry.annotation.EnableRetry
 import org.springframework.retry.backoff.ExponentialBackOffPolicy
+import org.springframework.retry.listener.RetryListenerSupport
 import org.springframework.retry.policy.AlwaysRetryPolicy
 import org.springframework.retry.support.RetryTemplate
+
+private val log = LoggerFactory.getLogger(DefaultRetryListenerSupport::class.java)!!
+
+class DefaultRetryListenerSupport: RetryListenerSupport() {
+
+    override fun <T : Any?, E : Throwable?> onError(context: RetryContext, callback: RetryCallback<T, E>?,
+                                                    throwable: Throwable) {
+        if (context.retryCount == 1) log.error("Error occurred. Start retrying...", throwable)
+        super.onError(context, callback, throwable)
+    }
+}
 
 @Configuration
 @EnableRetry
@@ -16,6 +31,7 @@ class RetryPolicyConfiguration {
         return RetryTemplate().apply {
             setBackOffPolicy(ExponentialBackOffPolicy())
             setRetryPolicy(AlwaysRetryPolicy())
+            registerListener(DefaultRetryListenerSupport())
         }
     }
 }
