4f9703aec3b9345f490bdc93b8a0233f3c7557d3
Valentin Stavetski <staveckij.v@gmail.com>
2018-07-04 18:22:26 +0300

    #206 refactor ticker service

--- rest-api/src/main/kotlin/fund/cyber/markets/api/rest/service/TickerService.kt
+++ rest-api/src/main/kotlin/fund/cyber/markets/api/rest/service/TickerService.kt
@@ -3,7 +3,6 @@ package fund.cyber.markets.api.rest.service
 import fund.cyber.markets.cassandra.model.CqlTokenTicker
 import fund.cyber.markets.cassandra.repository.TickerRepository
 import fund.cyber.markets.common.DAYS_TO_MILLIS
-import fund.cyber.markets.common.Intervals
 import fund.cyber.markets.common.MILLIS_TO_DAYS
 import fund.cyber.markets.common.convert
 import org.springframework.stereotype.Service
@@ -16,41 +15,31 @@ class TickerService(
 ) {
 
     fun getTickers(symbol: String, ts: Long, interval: Long, limit: Long): Flux<CqlTokenTicker> {
-        val epochDay = ts convert MILLIS_TO_DAYS
-        val iterations = (ts - (epochDay convert DAYS_TO_MILLIS) + interval * limit) / Intervals.DAY
+        var tickers = Flux.empty<CqlTokenTicker>()
 
-        val dateTs = Date(ts)
+        var tsVar = ts
         var limitVar = limit
 
-        var tickers = Flux.empty<CqlTokenTicker>()
-        for (iteration in 0..iterations) {
+        while (limitVar > 0) {
 
-            val iterationLimit = getLimit(iteration, epochDay, ts, interval, limitVar)
-            limitVar -= iterationLimit
+            val epochDay = tsVar convert MILLIS_TO_DAYS
+            var iterationLimit = (((epochDay + 1) convert DAYS_TO_MILLIS) - tsVar) / interval
+
+            if (iterationLimit > limitVar) {
+                iterationLimit = limitVar
+            }
 
             if (iterationLimit > 0) {
                 tickers = tickers
                     .concatWith(
-                        tickerRepository.find(symbol, (epochDay + iteration), dateTs, interval, iterationLimit)
+                        tickerRepository.find(symbol, epochDay, Date(ts), interval, iterationLimit)
                     )
             }
+
+            tsVar += interval * iterationLimit
+            limitVar -= iterationLimit
         }
 
         return tickers
-    }
-
-    private fun getLimit(iteration: Long, epochDay: Long, ts: Long, interval: Long, limit: Long): Long {
-
-        var limitResult = if (iteration == 0L) {
-            (((epochDay + 1) convert DAYS_TO_MILLIS) - ts) / interval
-        } else {
-            Intervals.DAY / interval
-        }
-
-        if (limitResult > limit) {
-            limitResult = limit
-        }
-
-        return limitResult
     }
 }
