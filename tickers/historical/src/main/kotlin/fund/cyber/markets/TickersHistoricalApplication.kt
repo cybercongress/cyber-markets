78827e299676141786281a94b721829abad390a2
Valentin Stavetski <staveckij.v@gmail.com>
2018-07-11 15:57:15 +0300

    #215 rename main class, update banners

--- tickers/historical/src/main/kotlin/fund/cyber/markets/TickersHistoricalApplication.kt
+++ tickers/historical/src/main/kotlin/fund/cyber/markets/TickersHistoricalApplication.kt
@@ -6,12 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.data.cassandra.CassandraDataAutoConfiguration
 
 @SpringBootApplication(exclude = [CassandraDataAutoConfiguration::class])
-class TickersApplication {
+class TickersHistoricalApplication {
 
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            val application = SpringApplication(TickersApplication::class.java)
+            val application = SpringApplication(TickersHistoricalApplication::class.java)
 
             val applicationContext = application.run(*args)
             val executor = applicationContext.getBean(TickerProcessorExecutor::class.java)
