a672d52f71586db9476583e048ce7a5fed89c8a8
Valentin Stavetski <staveckij.v@gmail.com>
2018-07-11 14:00:10 +0300

    #214 prevent code duplication

--- common-rest-api/src/main/kotlin/fund/cyber/markets/common/rest/Extentions.kt
+++ common-rest-api/src/main/kotlin/fund/cyber/markets/common/rest/Extentions.kt
@@ -5,15 +5,9 @@ import org.springframework.web.reactive.function.server.ServerResponse
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
-fun <T> Mono<T>.asServerResponse() = this.flatMap { obj -> ServerResponse.ok().body(BodyInserters.fromObject(obj)) }
-    .switchIfEmpty(ServerResponse.notFound().build())
+fun <T> Mono<T>.asServerResponse() = asServerResponseAllowEmpty().switchIfEmpty(ServerResponse.notFound().build())
 
-fun <T> Flux<T>.asServerResponse() = this.collectList().flatMap { obj ->
-    when {
-        obj.isNotEmpty() -> ServerResponse.ok().body(BodyInserters.fromObject(obj))
-        else -> Mono.empty<ServerResponse>()
-    }
-}.switchIfEmpty(ServerResponse.notFound().build())
+fun <T> Flux<T>.asServerResponse() = asServerResponseAllowEmpty().switchIfEmpty(ServerResponse.notFound().build())
 
 fun <T> Mono<T>.asServerResponseAllowEmpty() = this.flatMap { obj -> ServerResponse.ok().body(BodyInserters.fromObject(obj)) }
 
